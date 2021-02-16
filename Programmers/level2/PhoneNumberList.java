eb5059b83ccfdff53aae3b8920664f3e7350a4ae
haha-1122 <rkddbtjd123@gmail.com>
2021-02-17 02:26:11 +0900

    update

---
+++ Programmers/level2/PhoneNumberList.java
@@ -0,0 +1,26 @@
+package Programmers.level2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class PhoneNumberList {
+    public static boolean solution(String[] phone_book) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList(phone_book));
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                return o1.length() - o2.length();
+            }
+        });
+        for (int i = 0; i < list.size()-1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(j).startsWith(list.get(i)))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Programmers/level2/UsefulSquare.java
@@ -0,0 +1,18 @@
+package Programmers.level2;
+
+public class UsefulSquare {
+    public static long solution(int w, int h) {
+        double tan = Math.min(w, h) / (double) Math.max(w, h);
+        int cnt = 0;
+        for (int i = 1; i <= Math.max(w, h); i++) {
+            cnt++;
+            if (i == 1) continue;
+            if (Math.floor(i * tan) == i * tan) continue;
+            if (Math.floor((i-1)*tan) == (i-1)*tan) continue;
+            if (Math.ceil((i - 1) * tan) < i * tan) {
+                cnt++;
+            }
+        }
+        return (long) w * h - cnt;
+    }
+}
